687910304b97e9a490e4213b50f59e0b1e8f0787
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2022-11-08 09:02:30 +0100

    Create Flipper.h

---
+++ Flipper.h
@@ -0,0 +1,25 @@
+#pragma once
+
+enum FLIPPERSIDE {
+	LEFT,
+	RIGHT
+};
+
+class Flipper {
+public:
+	Flipper(FLIPPERSIDE side) {
+		switch (side)
+		{
+		case LEFT:
+
+			break;
+		case RIGHT:
+
+			break;
+		default:
+			break;
+		}
+	}
+
+
+};
