365687391b6c1cd32e54f2027c8b66026ecd5e4a
xGauss05 <jcacaynes@gmail.com>
2022-11-11 12:38:59 +0100

    Added a testing texture for the ball. - Also added rotation for the ball.

--- Ball.h
+++ Ball.h
@@ -11,18 +11,19 @@ class SDL_Texture;
 class Ball : Prop {
 public:
 	Ball(PropType type) : Prop(type) {
-		texture = App->textures->Load("pinball/Textures/temp_flipper.png");
-		x  = 236;
-		y = 364;
+		texture = App->textures->Load("pinball/Textures/test.png");
+		x = 50;
+		y = 50;
 		radius = 7;
 		pBody = App->physics->CreateCircle(x, y, radius);
 		pBody->body->SetType(b2BodyType::b2_dynamicBody);
-		
+
 		pBody->body->SetTransform(b2Vec2(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y)), 0.0f);
 	}
 
 	void Blit() {
-		App->renderer->Blit(texture, x, y);
+
+		App->renderer->Blit(texture, METERS_TO_PIXELS(pBody->body->GetPosition().x - radius), METERS_TO_PIXELS(pBody->body->GetPosition().y - radius), NULL, 1.0f, pBody->GetRotation());
 	}
 
 private:
@@ -31,6 +32,6 @@ private:
 	int radius;
 
 	PhysBody* pBody;
-	
+
 	SDL_Texture* texture;
 };
