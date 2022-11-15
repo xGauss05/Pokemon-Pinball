050e591c569f2c27184f480f04fce8eabbdd8e40
AriSevcik <99729800+AriSevcik@users.noreply.github.com>
2022-11-15 12:48:56 +0100

    Update Slingshot.h

--- Slingshot.h
+++ Slingshot.h
@@ -25,16 +25,16 @@ public:
 		{
 		case SlingPlace::SLEFT:
 			pBody2 = App->physics->CreateChain(0, 0, pLeft, 6);
-			pBody = App->physics->CreateRectangleSensor(78, 356, 1, 34);
+			pBody = App->physics->CreateRectangleSensor(78, 356, 0, 34);
 			pBody->body->SetTransform(pBody->body->GetPosition(), -0.6);
 			break;
 		case SlingPlace::SRIGHT:
 			pBody2 = App->physics->CreateChain(0, 0, pRight, 6);
-			pBody = App->physics->CreateRectangleSensor(161, 356, 1, 34);
+			pBody = App->physics->CreateRectangleSensor(160, 356, 0, 34);
 			pBody->body->SetTransform({ pBody->body->GetPosition() }, 0.6);
 			break;
 		}
-		force = 0.5;
+		force = 0.7;
 
 		pBody->body->SetType(b2BodyType::b2_staticBody);
 		pBody2->body->SetType(b2BodyType::b2_staticBody);
@@ -75,11 +75,11 @@ public:
 				switch (place)
 				{
 				case SlingPlace::SLEFT:
-					bodyB->body->SetLinearVelocity(b2Vec2{ 0,0 });
+					//bodyB->body->SetLinearVelocity(b2Vec2{ 0,0 });
 					bodyB->body->ApplyLinearImpulse({ (float32)(force * 0.866), (float32)(-force * 0.64) }, bodyB->body->GetPosition(), true);
 					break;
 				case SlingPlace::SRIGHT:
-					bodyB->body->SetLinearVelocity(b2Vec2{ 0,0 });
+					//bodyB->body->SetLinearVelocity(b2Vec2{ 0,0 });
 					bodyB->body->ApplyLinearImpulse({ (float32)(force * (-0.866)), (float32)(-force * 0.64) }, bodyB->body->GetPosition(), true);
 					break;
 				}
@@ -100,11 +100,11 @@ public:
 private:
 	int pLeft[6] = {
 		66, 360,
-		66, 340,
+		66, 345,
 		86, 370 };
 	int pRight[6] = {
 		152, 370,
-		172, 340,
+		172, 345,
 		172, 360 };
 
 	float32 force;
