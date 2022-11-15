d85dd1c3ca15fbdfecb828d91f7fc66d59e90536
xGauss05 <jcacaynes@gmail.com>
2022-11-15 22:41:40 +0100

    Changed initial position of the ball.

--- Ball.h
+++ Ball.h
@@ -16,27 +16,27 @@ public:
 	Ball(PropType type) : Prop(type) {
 		texture = App->textures->Load("pinball/Textures/ball.png");
 		ballSfx = App->audio->LoadFx("pinball/Sounds/ball_collides.wav");
-		x = 100;
-		y = 100;
-		radius = 8;
-			
-		spawn = iPoint(243, 350);
-		afterRelease = iPoint(178,34);
 
-		pBody = App->physics->CreateCircle(x, y, radius);
+		radius = 8;
+
+		spawn = iPoint(243, 350);
+		
+		afterRelease = iPoint(178, 34);
+
+		pBody = App->physics->CreateCircle(spawn.x, spawn.y, radius);
 		pBody->listener = (Module*)App->pManager;
 
 		pBody->body->ResetMassData();
 
 		b2MassData* data = new b2MassData;
-		
-		data->center= b2Vec2(0.0f, 0);
+
+		data->center = b2Vec2(0.0f, 0.0f);
 		data->mass = 0.1f;
 		pBody->body->SetMassData(data);
 		pBody->body->SetBullet(true);
-	
+
 		pBody->body->SetType(b2BodyType::b2_dynamicBody);
-		pBody->body->SetTransform(b2Vec2(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y)), 0.0f);
+		pBody->body->SetTransform(b2Vec2(PIXEL_TO_METERS(spawn.x), PIXEL_TO_METERS(spawn.y)), 0.0f);
 		pBody->prop = this;
 	}
 
@@ -46,25 +46,34 @@ public:
 
 	void Blit() {
 		App->renderer->Blit(texture,
-			METERS_TO_PIXELS(pBody->body->GetPosition().x - 8),
-			METERS_TO_PIXELS(pBody->body->GetPosition().y - 8),
+			METERS_TO_PIXELS(pBody->body->GetPosition().x - radius),
+			METERS_TO_PIXELS(pBody->body->GetPosition().y - radius),
 			NULL,
 			1.0f,
 			pBody->GetRotation());
 	}
-
-	bool Update() {
+	bool PreUpdate() {
 		if (switchLayer != -1) {
 			App->scene->switchLayer(switchLayer);
 			switchLayer = -1;
 		}
 
-		if (App->input->GetKey(SDL_SCANCODE_0) == KEY_DOWN) { 
+		if (METERS_TO_PIXELS(pBody->body->GetPosition().y) >= SCREEN_HEIGHT + 50) {
+			TeleportTo(spawn);
+			App->scene->switchLayer(2);
+		}
+	}
+
+	bool Update() {
+		
+
+		if (App->input->GetKey(SDL_SCANCODE_0) == KEY_DOWN) {
 			iPoint position;
 			position.x = App->input->GetMouseX();
 			position.y = App->input->GetMouseY();
-			TeleportTo(position); 
+			TeleportTo(position);
 		}
+
 		if (lose) {
 			lose = false;
 			TeleportTo(spawn);
@@ -74,14 +83,11 @@ public:
 		if (release) {
 			release = false;
 			TeleportTo(afterRelease);
-			pBody->body->ApplyForceToCenter({-30, 0}, true);
+			pBody->body->ApplyForceToCenter({ -30, 0 }, true);
 
 		}
 
-		if (METERS_TO_PIXELS(pBody->body->GetPosition().y) >= SCREEN_HEIGHT+50) {
-			TeleportTo(spawn);
-			App->scene->switchLayer(2);
-		}
+		
 
 		return true;
 	}
@@ -131,12 +137,12 @@ public:
 				break;
 			case PropType::SLINGSHOT_LEFT:
 				App->scene->currentScore += SLINGSHOT_SCORE;
-			
+
 				LOG("Ball collided SLINGSHOT_LEFT");
 				break;
 			case PropType::SLINGSHOT_RIGHT:
 				App->scene->currentScore += SLINGSHOT_SCORE;
-				
+
 				LOG("Ball collided SLINGSHOT_RIGHT");
 				break;
 			case PropType::SENSOR_EVO_BOT:
@@ -145,7 +151,7 @@ public:
 				break;
 			default:
 				LOG("Ball collided ???");
-				
+
 				break;
 			}
 		}
@@ -156,7 +162,7 @@ public:
 			switch (otherBody->prop->type) {
 			case PropType::SENSOR_SPRING_IN:
 				LOG("Ball END collided SPRING");
-					release = true;
+				release = true;
 				break;
 			}
 		}
@@ -176,8 +182,7 @@ public:
 	}
 
 private:
-	int x;
-	int y;
+
 	int radius;
 
 	int switchLayer = -1;
