be7483cc988d5c2f35b817e615f8a8f17e5b4f30
Ethanm-0371 <99729919+Ethanm-0371@users.noreply.github.com>
2022-11-17 10:25:48 +0100

    Fixed new Debug menu. Added remaining debug functionallities.

--- ModuleDebug.cpp
+++ ModuleDebug.cpp
@@ -33,12 +33,6 @@ update_status ModuleDebug::Update() {
 
 		if (currentScreen == Screen::HOME)
 		{
-			/*if (App->input->GetKey(SDL_SCANCODE_O) == KEY_DOWN && App->scene->ballMultiplier < 4)
-			App->scene->ballMultiplier++;
-
-		if (App->input->GetKey(SDL_SCANCODE_L) == KEY_DOWN && App->scene->ballMultiplier > 1)
-			App->scene->ballMultiplier--;*/
-
 			if (App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN)
 				time = true;
 
@@ -82,6 +76,12 @@ update_status ModuleDebug::Update() {
 		if (sprites == true)
 		{
 			currentScreen = Screen::SPRITES;
+
+			if (App->input->GetKey(SDL_SCANCODE_HOME) == KEY_DOWN && App->scene->ballMultiplier < 4)
+			App->scene->ballMultiplier++;
+
+			if (App->input->GetKey(SDL_SCANCODE_END) == KEY_DOWN && App->scene->ballMultiplier > 1)
+				App->scene->ballMultiplier--;
 		}
 		if (colliders == true)
 		{
@@ -97,6 +97,38 @@ update_status ModuleDebug::Update() {
 			currentScreen = Screen::HOME;
 			variables = false; time = false; gravity = false; sprites = false; colliders = false;
 		}
+
+		if (App->physics->mouse_body != nullptr && App->physics->mouse_joint != nullptr)
+		{
+			if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT)
+			{
+				// Get new mouse position and re-target mouse_joint there
+				b2Vec2 mousePosition;
+				mousePosition.x = PIXEL_TO_METERS(App->input->GetMouseX());
+				mousePosition.y = PIXEL_TO_METERS(App->input->GetMouseY());
+				App->physics->mouse_joint->SetTarget(mousePosition);
+			}
+		}
+		if (App->physics->mouse_body != nullptr && App->physics->mouse_joint != nullptr)
+		{
+			if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_UP)
+			{
+				// Tell Box2D to destroy the mouse_joint
+				App->physics->world->DestroyJoint(App->physics->mouse_joint);
+
+				App->physics->mouse_joint = nullptr;
+				App->physics->mouse_body = nullptr;
+			}
+		}
+	}
+	if (App->physics->mouse_body != nullptr && App->physics->mouse_joint != nullptr && !debug ||
+		App->physics->mouse_body != nullptr && App->physics->mouse_joint != nullptr && currentScreen != Screen::COLLIDERS)
+	{
+		// Tell Box2D to destroy the mouse_joint
+		App->physics->world->DestroyJoint(App->physics->mouse_joint);
+
+		App->physics->mouse_joint = nullptr;
+		App->physics->mouse_body = nullptr;
 	}
 
 	return UPDATE_CONTINUE;
@@ -116,146 +148,129 @@ void ModuleDebug::DebugDraw() {
 	//Physics objects
 	// This will iterate all objects in the world and draw the circles
 	// You need to provide your own macro to translate meters to pixels
-	for (b2Body* b = App->physics->world->GetBodyList(); b; b = b->GetNext())
+	if (colliders)
 	{
-		for (b2Fixture* f = b->GetFixtureList(); f; f = f->GetNext())
+		for (b2Body* b = App->physics->world->GetBodyList(); b; b = b->GetNext())
 		{
-			switch (f->GetType())
+			for (b2Fixture* f = b->GetFixtureList(); f; f = f->GetNext())
 			{
-				// Draw circles ------------------------------------------------
-			case b2Shape::e_circle:
-			{
-				b2CircleShape* shape = (b2CircleShape*)f->GetShape();
-				b2Vec2 pos = f->GetBody()->GetPosition();
-				App->renderer->DrawCircle(METERS_TO_PIXELS(pos.x), METERS_TO_PIXELS(pos.y), METERS_TO_PIXELS(shape->m_radius), 255, 255, 255);
-			}
-			break;
-
-			// Draw polygons ------------------------------------------------
-			case b2Shape::e_polygon:
-			{
-				b2PolygonShape* polygonShape = (b2PolygonShape*)f->GetShape();
-				int32 count = polygonShape->GetVertexCount();
-				b2Vec2 prev, v;
-
-				for (int32 i = 0; i < count; ++i)
+				switch (f->GetType())
 				{
-					v = b->GetWorldPoint(polygonShape->GetVertex(i));
-					if (i > 0)
-						App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 255, 50, 50);
+					// Draw circles ------------------------------------------------
+				case b2Shape::e_circle:
+				{
+					b2CircleShape* shape = (b2CircleShape*)f->GetShape();
+					b2Vec2 pos = f->GetBody()->GetPosition();
+					App->renderer->DrawCircle(METERS_TO_PIXELS(pos.x), METERS_TO_PIXELS(pos.y), METERS_TO_PIXELS(shape->m_radius), 255, 255, 255);
+				}
+				break;
 
-					prev = v;
+				// Draw polygons ------------------------------------------------
+				case b2Shape::e_polygon:
+				{
+					b2PolygonShape* polygonShape = (b2PolygonShape*)f->GetShape();
+					int32 count = polygonShape->GetVertexCount();
+					b2Vec2 prev, v;
+
+					for (int32 i = 0; i < count; ++i)
+					{
+						v = b->GetWorldPoint(polygonShape->GetVertex(i));
+						if (i > 0)
+							App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 255, 50, 50);
+
+						prev = v;
+					}
+
+					v = b->GetWorldPoint(polygonShape->GetVertex(0));
+					App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 255, 50, 50);
+				}
+				break;
+
+				// Draw chains contour -------------------------------------------
+				case b2Shape::e_chain:
+				{
+					b2ChainShape* shape = (b2ChainShape*)f->GetShape();
+					b2Vec2 prev, v;
+
+					for (int32 i = 0; i < shape->m_count; ++i)
+					{
+						v = b->GetWorldPoint(shape->m_vertices[i]);
+						if (i > 0)
+							App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 0, 255, 0);
+						prev = v;
+					}
+
+					v = b->GetWorldPoint(shape->m_vertices[0]);
+					App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 0, 255, 0);
+				}
+				break;
+
+				// Draw a single segment(edge) ----------------------------------
+				case b2Shape::e_edge:
+				{
+					b2EdgeShape* shape = (b2EdgeShape*)f->GetShape();
+					b2Vec2 v1, v2;
+
+					v1 = b->GetWorldPoint(shape->m_vertex0);
+					v1 = b->GetWorldPoint(shape->m_vertex1);
+					App->renderer->DrawLine(METERS_TO_PIXELS(v1.x), METERS_TO_PIXELS(v1.y), METERS_TO_PIXELS(v2.x), METERS_TO_PIXELS(v2.y), 0, 0, 255);
+				}
+				break;
 				}
 
-				v = b->GetWorldPoint(polygonShape->GetVertex(0));
-				App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 255, 50, 50);
-			}
-			break;
-
-			// Draw chains contour -------------------------------------------
-			case b2Shape::e_chain:
-			{
-				b2ChainShape* shape = (b2ChainShape*)f->GetShape();
-				b2Vec2 prev, v;
-
-				for (int32 i = 0; i < shape->m_count; ++i)
+				if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_DOWN)
 				{
-					v = b->GetWorldPoint(shape->m_vertices[i]);
-					if (i > 0)
-						App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 0, 255, 0);
-					prev = v;
-				}
+					// test if the current body contains mouse position
+					b2Vec2 p = { PIXEL_TO_METERS(App->input->GetMouseX()), PIXEL_TO_METERS(App->input->GetMouseY()) };
+					if (f->GetShape()->TestPoint(b->GetTransform(), p) == true)
+					{
 
-				v = b->GetWorldPoint(shape->m_vertices[0]);
-				App->renderer->DrawLine(METERS_TO_PIXELS(prev.x), METERS_TO_PIXELS(prev.y), METERS_TO_PIXELS(v.x), METERS_TO_PIXELS(v.y), 0, 255, 0);
-			}
-			break;
+						// If a body was selected we will attach a mouse joint to it
+						// so we can pull it around
 
-			// Draw a single segment(edge) ----------------------------------
-			case b2Shape::e_edge:
-			{
-				b2EdgeShape* shape = (b2EdgeShape*)f->GetShape();
-				b2Vec2 v1, v2;
+						//Create a mouse joint using mouse_joint class property
 
-				v1 = b->GetWorldPoint(shape->m_vertex0);
-				v1 = b->GetWorldPoint(shape->m_vertex1);
-				App->renderer->DrawLine(METERS_TO_PIXELS(v1.x), METERS_TO_PIXELS(v1.y), METERS_TO_PIXELS(v2.x), METERS_TO_PIXELS(v2.y), 0, 0, 255);
-			}
-			break;
-			}
+						// We need to keep this body throughout several game frames; you cannot define it as a local variable here. 
+						App->physics->mouse_body = b;
 
-			if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_DOWN)
-			{
-				// test if the current body contains mouse position
-				b2Vec2 p = { PIXEL_TO_METERS(App->input->GetMouseX()), PIXEL_TO_METERS(App->input->GetMouseY()) };
-				if (f->GetShape()->TestPoint(b->GetTransform(), p) == true)
-				{
+						// Get current mouse position
+						b2Vec2 mousePosition;
+						mousePosition.x = p.x;
+						mousePosition.y = p.y;
 
-					// If a body was selected we will attach a mouse joint to it
-					// so we can pull it around
+						// Define new mouse joint
+						b2MouseJointDef def;
+						def.bodyA = App->physics->worldBody; // First body must be a static ground
+						def.bodyB = App->physics->mouse_body; // Second body will be the body to attach to the mouse
+						def.target = mousePosition; // The second body will be pulled towards this location
+						def.dampingRatio = 0.5f; // Play with this value
+						def.frequencyHz = 2.0f; // Play with this value
+						def.maxForce = 200.0f * App->physics->mouse_body->GetMass(); // Play with this value
 
-					//Create a mouse joint using mouse_joint class property
-
-					// We need to keep this body throughout several game frames; you cannot define it as a local variable here. 
-					App->physics->mouse_body = b;
-
-					// Get current mouse position
-					b2Vec2 mousePosition;
-					mousePosition.x = p.x;
-					mousePosition.y = p.y;
-
-					// Define new mouse joint
-					b2MouseJointDef def;
-					def.bodyA = App->physics->worldBody; // First body must be a static ground
-					def.bodyB = App->physics->mouse_body; // Second body will be the body to attach to the mouse
-					def.target = mousePosition; // The second body will be pulled towards this location
-					def.dampingRatio = 0.5f; // Play with this value
-					def.frequencyHz = 2.0f; // Play with this value
-					def.maxForce = 200.0f * App->physics->mouse_body->GetMass(); // Play with this value
-
-					// Add the new mouse joint into the World
-					App->physics->mouse_joint = (b2MouseJoint*)App->physics->world->CreateJoint(&def);
+						// Add the new mouse joint into the World
+						App->physics->mouse_joint = (b2MouseJoint*)App->physics->world->CreateJoint(&def);
+					}
 				}
 			}
 		}
-	}
-
-	if (App->physics->mouse_body != nullptr && App->physics->mouse_joint != nullptr)
-	{
-		if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT)
+		if (App->physics->mouse_body != nullptr && App->physics->mouse_joint != nullptr)
 		{
-			// Get new mouse position and re-target mouse_joint there
-			b2Vec2 mousePosition;
-			mousePosition.x = PIXEL_TO_METERS(App->input->GetMouseX());
-			mousePosition.y = PIXEL_TO_METERS(App->input->GetMouseY());
-			App->physics->mouse_joint->SetTarget(mousePosition);
-
-			// Draw a red line between both anchor points
-			App->renderer->DrawLine(METERS_TO_PIXELS(App->physics->mouse_body->GetPosition().x), METERS_TO_PIXELS(App->physics->mouse_body->GetPosition().y), App->input->GetMouseX(), App->input->GetMouseY(), 255, 0, 0);
-		}
-	}
-
-	if (App->physics->mouse_body != nullptr && App->physics->mouse_joint != nullptr)
-	{
-		if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_UP)
-		{
-			// Tell Box2D to destroy the mouse_joint
-			App->physics->world->DestroyJoint(App->physics->mouse_joint);
-
-			App->physics->mouse_joint = nullptr;
-			App->physics->mouse_body = nullptr;
+			if (App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT)
+			{
+				// Draw a red line between both anchor points
+				App->renderer->DrawLine(METERS_TO_PIXELS(App->physics->mouse_body->GetPosition().x), METERS_TO_PIXELS(App->physics->mouse_body->GetPosition().y), App->input->GetMouseX(), App->input->GetMouseY(), 255, 0, 0);
+			}
 		}
 	}
 
 	SDL_Rect bg;
-	if (time)			{ bg = { 5,75,500,185 }; }
-	else if (gravity)	{ bg = { 5,75,500,170 }; }
-	else if (variables)	{ bg = { 5,75,500,110 }; }
-	else if (sprites)	{ bg = { 5,75,500,25 }; }
-	else if (colliders)	{ bg = { 5,75,500,25 }; }
-	else				{ bg = { 5,75,500,145 }; }
-	SDL_SetRenderDrawColor(App->renderer->renderer, 0, 0, 0, 175);
-	SDL_RenderFillRect(App->renderer->renderer, &bg);
+	if (time)			{ bg = { 2,38,251,92 }; }
+	else if (gravity)	{ bg = { 2,38,251,82 }; }
+	else if (sprites)	{ bg = { 2,38,251,62 }; }
+	else if (colliders)	{ bg = { 2,38,251,22 }; }
+	else if (variables) { bg = { 2,38,251,52 }; }
+	else				{ bg = { 2,38,252,72 }; }
+	App->renderer->DrawQuad(bg, 0, 0, 0, 125, true);
 
 	switch (currentScreen)
 	{
@@ -300,12 +315,16 @@ void ModuleDebug::DebugDraw() {
 		App->fonts->BlitText(5, 110, 0, "PRESS BACKSPACE TO GO BACK");
 		break;
 
-	case Screen::COLLIDERS:
-		App->fonts->BlitText(5, 40, 0, "PRESS BACKSPACE TO GO BACK");
+	case Screen::SPRITES:
+		App->fonts->BlitText(5, 40, 0, "SPRITE OPTIONS");
+		App->fonts->BlitText(5, 60, 0, "PRESS HOME TO INCREASE AND");
+		App->fonts->BlitText(5, 70, 0, "END TO DECREASE THE BALL LEVEL");
+		App->fonts->BlitText(5, 90, 0, "PRESS BACKSPACE TO GO BACK");
 		break;
 
-	case Screen::SPRITES:
-		App->fonts->BlitText(5, 40, 0, "PRESS BACKSPACE TO GO BACK");
+	case Screen::COLLIDERS:
+		App->fonts->BlitText(5, 40, 0, "CLICK ON THE BALL TO DRAG IT");
+		App->fonts->BlitText(5, 50, 0, "PRESS BACKSPACE TO GO BACK");
 		break;
 
 	case Screen::VARIABLES:
